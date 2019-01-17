f5a49cdd4b9c6d3bd483bce1f229eef41caf00f5
lenmorld <lenmorld@gmail.com>
2019-01-17 00:15:13 -0500

    c1.1 server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+// import built-in Node package
+var http = require('http');
+var port = 4000;
+
+ var server = http.createServer(function (req, res) { // Callback function
+    // Response header
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    // send response
+    res.end("Hello World\n");
+});
+
+ server.listen(port, function () { // Callback function
+    console.log(`Server listening at ${port}`);
+});
