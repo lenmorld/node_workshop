7221ba929b53c1ee99f9c200a0a9b703706a5d20
lenmorld <lenmorld@gmail.com>
2019-01-21 22:23:02 -0500

    c1.3 Hello World HTML

--- server.js
+++ server.js
@@ -6,17 +6,8 @@ var server = http.createServer(function (req, res) { // Callback function
     // Response header
     res.writeHead(200, { "Content-Type": "text/html" });
 
-    // JSON object
-    var song = {
-        id: 12345,
-        favorite: false,
-        title: "Hello World",
-        artist: "Node programmer",
-        album: "Node EP"
-    };
-
-   // send HTML response to client
-   res.end("<h1>Hello World</h1>");
+    // send HTML response to client
+    res.end("<h1>Hello World</h1>");
 });
 
 server.listen(port, function () { // Callback function
