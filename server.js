4c41ca2f093e6ddfce2f625a74bd8b310942cac0
lenmorld <lenmorld@gmail.com>
2019-01-21 22:10:44 -0500

    c1.2 Hello world JSON

--- server.js
+++ server.js
@@ -2,13 +2,23 @@
 var http = require('http');
 var port = 4000;
 
- var server = http.createServer(function (req, res) { // Callback function
+var server = http.createServer(function (req, res) { // Callback function
     // Response header
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    // send response
-    res.end("Hello World\n");
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    // JSON object
+    var song = {
+        id: 12345,
+        favorite: false,
+        title: "Hello World",
+        artist: "Node programmer",
+        album: "Node EP"
+    };
+
+   // send JSON response to client
+   res.end(JSON.stringify(song));      // JSON.stringify({a: 1}) -> '{"a":1}'
 });
 
- server.listen(port, function () { // Callback function
+server.listen(port, function () { // Callback function
     console.log(`Server listening at ${port}`);
 });
