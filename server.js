7ac3da8124f03f79bfa36e2ff6510cfd16750f8e
lenmorld <lenmorld@gmail.com>
2019-01-22 00:14:40 -0500

    c1.7 ejs templates basics

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var server = express();
 
 var port = 4000;
 
+// set the view engine to ejs
+server.set('view engine', 'ejs');
+
 server.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
  });
@@ -12,6 +15,16 @@ server.get("/", function(req, res) {
 server.get("/json", function(req, res) {
     res.send((JSON.stringify({ name: "Lenny" })));
 });
+
+// template pages
+
+server.get("/about", function(req, res) {
+    res.render('about');
+ });
+
+ server.get("/info", function(req, res) {
+    res.render('info', { message: 'Hello world' });
+ });
 
 server.listen(port, function () { // Callback function
     console.log(`Server listening at ${port}`);
