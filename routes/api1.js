aeca597cf0cc58cfa7e25485678d5d10cb019b9f
lenmorld <lenmorld@gmail.com>
2019-01-31 00:19:41 -0500

    c4.3 req.query

--- routes/api1.js
+++ routes/api1.js
@@ -13,11 +13,22 @@ api1.get("/users/:id", function (req, res) {
 	});
 });
 
+
+//?description=____&location=_____&titleContains=___&companyContains=___
 api1.get("/jobs", function (req, res) {
 	var description = req.query.description;
 	var location = req.query.location;
+	var titleContains = req.query.titleContains;
+	var year = req.query.year;
+
 	axios(`https://jobs.github.com/positions.json?description=${description}&location=${location}`).then(response => {
-		res.json(response.data);
+		var results = response.data;
+
+		var matches = results.filter(r => {
+			return r.title.toLowerCase().includes(titleContains.toLowerCase()) && r.created_at.includes(year);
+		})
+
+		res.json(matches);
 	}).catch(err => {
 		throw err;
 	});
