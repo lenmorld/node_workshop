6aa24bf732f30db37e0aaa18c7759551365e42a0
lenmorld <lenmorld@gmail.com>
2019-02-02 16:10:20 -0500

    delete flow

--- server.js
+++ server.js
@@ -138,6 +138,25 @@ server.get("/edit/:id", function(req, res) {
     res.render("edit", { item: item });
 });
 
+// we don't need a form for Delete, so no need for method-override
+// what we do is a GET request but replicaing the logic in server.delete()
+server.get("/delete/:id", function(req, res) {
+    var item_id = req.params.id;
+    console.log("Delete item with id: ", item_id);
+
+    // filter list copy, by excluding item to delete
+    var filtered_list = data.list.filter(function(item) {
+        return item.id !== item_id;
+    });
+
+    // replace old list with new one
+    data.list = filtered_list; 
+
+    // this is only used by the PLaylist page for now
+    res.render("playlist", {items: data.list});
+});
+
+
 server.listen(port, function () { // Callback function
     console.log(`Server listening at ${port}`);
 });
