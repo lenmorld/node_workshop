972c6638d627984e974094e0155c5ba4fe636615
lenmorld <lenmorld@gmail.com>
2019-02-03 17:06:49 -0500

    refactor SpotifyHelper out into utils module v2

--- routes/api3.js
+++ routes/api3.js
@@ -7,44 +7,10 @@ var SpotifyHelper = require('../utils/SpotifyHelper');
 // /songs?search=___
 api3.get("/songs", function (req, res) {
 	var search = req.query.search;
-	console.log(`[SPOTIFY] : searching ${search}...`);
-	SpotifyHelper.getAccessToken().then(function (access_token) {
-		// send a GET request for search, attaching the access_token
-		//				to prove we are verified API user
-
-		var _url = `https://api.spotify.com/v1/search?query=${search}&type=track`;
-
-		axios({
-			method: 'GET',
-			url: _url,
-			headers: {
-				"Authorization": `Bearer ${access_token}`,
-				"Accept": "application/json"
-			}
-		}).then(function (_res) {
-			// inspect response data
-			// console.log(`search response: ${JSON.stringify(_res.data)}`);
-			
-			// prepare data so we only get the attributes we need
-			var search_results = _res.data.tracks.items;
-			var squashed_results = search_results.map(function(track) {
-					return {
-							id: track.id,
-							artist: track.artists[0].name,
-							album: track.album.name,
-							title: track.name
-					};
-			});
-			console.log(squashed_results);
-			// res.send(_res.data.tracks.items);
-			res.json(squashed_results);
-			
-		}).catch(function (err) {
-			throw err;
-		});
-
-		// testing: send search, access_token to client
-		// res.send({ search: search, access_token: access_token });
+	SpotifyHelper.searchTrack(search).then(_res => {
+		res.json(_res);
+	}).catch(err => {
+		throw err;
 	});
 });
 
--- utils/SpotifyHelper.js
+++ utils/SpotifyHelper.js
@@ -5,7 +5,7 @@ var qs = require('qs');
 var base64_auth_string = "NmNhYTc4N2YzZWY4NGQ0M2I1ZDVhYmQ0ZWY4ZjUyMjg6MjRlYjI4MzFiNjI0NDM3OGI2ODliOTc4OGEyZjhkMDc=";
 var saved_access_token = null;
 
-exports.getAccessToken = function() {
+function getAccessToken() {
 
 	return new Promise(function (resolve, reject) {
 		if (saved_access_token) {
@@ -37,4 +37,54 @@ exports.getAccessToken = function() {
 			});
 		}
 	});
+}
+
+exports.searchTrack = function (search) {
+	return new Promise(function (resolve, reject) {
+		console.log(`[SPOTIFY] : searching ${search}...`);
+		getAccessToken().then(function (access_token) {
+			// send a GET request for search, attaching the access_token
+			//				to prove we are verified API user
+
+			var _url = `https://api.spotify.com/v1/search?query=${search}&type=track`;
+
+			axios({
+				method: 'GET',
+				url: _url,
+				headers: {
+					"Authorization": `Bearer ${access_token}`,
+					"Accept": "application/json"
+				}
+			}).then(function (_res) {
+				// inspect response data
+				// console.log(`search response: ${JSON.stringify(_res.data)}`);
+
+				// prepare data so we only get the attributes we need
+				var search_results = _res.data.tracks.items;
+				var squashed_results = search_results.map(function (track) {
+					return {
+						id: track.id,
+						artist: track.artists[0].name,
+						album: track.album.name,
+						title: track.name
+					};
+				});
+				console.log(squashed_results);
+				// res.send(_res.data.tracks.items);
+				// res.json(squashed_results);
+
+				resolve(squashed_results);
+
+			}).catch(function (err) {
+				// throw err;
+				reject(err);
+			});
+
+			// testing: send search, access_token to client
+			// res.send({ search: search, access_token: access_token });
+		});
+
+	});
+
+
 }
