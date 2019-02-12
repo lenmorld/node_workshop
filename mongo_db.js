142cd1f4075e942c190119358e0c654cb2284c97
lenmorld <lenmorld@gmail.com>
2019-02-12 00:32:05 -0500

    Revert "c3.2 add callback version"
    This reverts commit 49f33781f2cac6f6e212d5371d073ad71dfd82a8.

--- mongo_db.js
+++ mongo_db.js
@@ -2,20 +2,6 @@
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
-// CALLBACK VERSION
-// function init_db(db_connection_url, successCallback, failureCallback) {
-//     MongoClient.connect(db_connection_url, function(err, db_instance) {
-//         if (err) {
-//             console.log(`[MongoDB connection] ERROR: ${err}`);
-//             failureCallback(err);        // this should be "caught" by the calling function
-//         } else {
-//             console.log("[MongoDB connection] SUCCESS");
-//             successCallback(db_instance);
-//         }
-//     });
-// }
-
-// PROMISE VERSION
 function init_db(db_connection_url) {
     // function returns a promise that resolves to a mongodb instance
     return new Promise(function(resolve, reject) {
