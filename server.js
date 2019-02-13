09062974c9133862c14b2e44e7ae7ed16df8cbc2
lenmorld <lenmorld@gmail.com>
2019-02-13 00:01:06 -0500

    simple client-server chat using WS

--- server.js
+++ server.js
@@ -4,14 +4,34 @@ const server = express();
 
 const port = 4000;
 
+// websocket setup
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+let client_ctr = 0;
+
+wss.on('connection', ws => {
+    client_ctr++;
+    console.log(`CLIENT ${client_ctr} connected`);
+    ws.on('message', message => {
+        console.log(`CLIENT ${client_ctr} said: ${message}`);
+
+        if (message === "HELLO") {
+            ws.send("WELCOME to the WS server!")
+        } else {
+            ws.send(`${message.toUpperCase()}`);
+        }
+    });
+});
+
+// routes
 server.get("/", (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 server.get("/json", ({ res }) => {
-	res.send((JSON.stringify({ name: "Lenny" })));
+    res.send((JSON.stringify({ name: "Lenny" })));
 });
 
 server.listen(port, () => { // Callback function in ES6
-	console.log(`Server listening at ${port}`);
+    console.log(`Server listening at ${port}`);
 });
