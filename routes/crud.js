3e071235348f9ccb0d9f8b39a13c5c416ae132b2
lenmorld <lenmorld@gmail.com>
2019-02-24 18:36:58 -0500

    GET /items

--- routes/crud.js
+++ routes/crud.js
@@ -7,4 +7,9 @@ const data = require('../data');
 console.log(data.list);
 console.log(`song: ${data.list[0].title} by ${data.list[0].artist}`);
 
+// CRUD RESTful API routes
+server.get("/items", (req, res) => {
+	res.json(data.list);
+});
+
 module.exports = server;
