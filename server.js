8f241c2dc9f375ac18c4cdcb34a3d439b2bc4031
lenmorld <lenmorld@gmail.com>
2019-02-24 00:37:07 -0500

    es6

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // import built-in Node package
-var http = require('http');
-var port = 4000;
+const http = require('http');
+const port = 4000;
 
- var server = http.createServer(function (req, res) { // Callback function
-    // Response header
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    // send response
-    res.end("Hello World\n");
+const server = http.createServer(function (req, res) { // Callback function
+	// Response header
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	// send response
+	res.end("Hello World\n");
 });
 
- server.listen(port, function () { // Callback function
-    console.log(`Server listening at ${port}`);
+server.listen(port, function () { // Callback function
+	console.log(`Server listening at ${port}`);
 });
