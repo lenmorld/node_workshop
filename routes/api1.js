890c7b21a1551b88277042c9d315c9aebca26c4c
lenmorld <lenmorld@gmail.com>
2019-02-26 23:44:58 -0500

    es6

--- routes/api1.js
+++ routes/api1.js
@@ -1,11 +1,11 @@
-var axios = require('axios');
-var express = require('express');
-var api1 = express.Router();
+const axios = require('axios');
+const express = require('express');
+const api1 = express.Router();
 
 // external API routes
 
 api1.get("/users/:id", (req, res) => {
-	var id = req.params.id;
+	const id = req.params.id;
 	axios(`https://jsonplaceholder.typicode.com/users/${id}`).then(response => {
 		res.json(response.data);
 	}).catch(err => {
