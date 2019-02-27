93b36c2a47e5369c031ba4377c516bc8781db9ec
lenmorld <lenmorld@gmail.com>
2019-02-26 23:48:15 -0500

    es6

--- routes/api2.js
+++ routes/api2.js
@@ -1,14 +1,14 @@
-var axios = require('axios');
-var express = require('express');
-var api2 = express.Router();
+const axios = require('axios');
+const express = require('express');
+const api2 = express.Router();
 
-var OMDB_API_KEY = `71050af8`;
+const OMDB_API_KEY = `71050af8`;
 
 // external API routes
 
 // /movies?search=____
 api2.get("/movies", function (req, res) {
-	var search = req.query.search;
+	const search = req.query.search;
 	axios(`http://www.omdbapi.com/?t=${search}&apikey=${OMDB_API_KEY}`).then(response => {
 		res.json(response.data);
 	}).catch(err => {
