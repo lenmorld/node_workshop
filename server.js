60e415b26c39fe28ad71b3476097f0375c919a0c
lenmorld <lenmorld@gmail.com>
2020-03-18 20:18:31 -0400

    add details

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ server.get("/api/items", ({ res }) => {
 	res.send(
 		JSON.stringify(
 			[
-				{ id: 1, name: "toilet paper", price: 199.99 },
-				{ id: 2, name: "hand sanitizer", price: 299.99 }
+				{ id: 1, name: "toilet paper", brand: "X", picture: "🧻", price: 199.99 },
+				{ id: 2, name: "hand sanitizer", brand: "Y", picture: "🧴", price: 299.99 }
 			]
 		)
 	);
