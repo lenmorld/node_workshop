2b8d61db999221a3faab79e09c55320f0ffe1c7f
lenmorld <lenmorld@gmail.com>
2020-03-24 22:12:08 -0400

    move /products in its router

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const body_parser = require('body-parser');
 server.use(body_parser.json()); // parse JSON (application/json content-type)
 
 let users = require('./users');
-let products = require('./products');
+// let products = require('./products');
 let foods = require('./foods');
+
+const productsRouter = require('./routes/products');
 
 console.log(users[0]);
 
@@ -31,106 +33,7 @@ server.get("/json", ({ res }) => {
 });
 
 // # Products REST API
-
-// GET all products
-server.get("/products", ({ res }) => {
-	res.json(products);
-});
-
-// GET one product identified by id
-server.get("/products/:id", (req, res) => {
-	const productId = Number(req.params.id);
-	const product = products.find(_product => _product.id === productId);
-	if (!product) {
-		res.json({
-			error: "Product not found"
-		})
-	} else {
-		// SUCCESS!
-		res.json(product);
-	}
-});
-
-// POST (create) a product 
-server.post("/products", (req, res) => {
-	const product = req.body;
-	console.log('Adding new product: ', product);
-
-	if (!product.id) {
-		res.json({
-			error: "id required"
-		})
-	} else if (products.find(_product => _product.id === product.id)) {
-		res.json({
-			error: "Product already exists"
-		})
-	} else {
-		// SUCCESS!
-		// add new product to products array
-		products.push(product)
-
-		// return updated list
-		res.json(products);
-	}
-});
-
-// PUT (update) a product
-server.put("/products/:id", (req, res) => {
-	const productId = Number(req.params.id);
-	const updatedProduct = req.body;
-	console.log("Editing product ", productId, " to be ", updatedProduct);
-
-	const updatedListProducts = [];
-	let found = false;
-
-	// loop through list to find and replace one product
-	products.forEach(oldProduct => {
-		if (oldProduct.id === productId) {
-			found = true;
-			// spread oldProduct properties
-			// then overwrite with product properties
-			const modifiedProduct = {
-				...oldProduct,
-				...updatedProduct
-			};
-			updatedListProducts.push(modifiedProduct);
-		} else {
-			updatedListProducts.push(oldProduct);
-		}
-	});
-
-	if (!found) {
-		res.json({
-			error: 'Product not found'
-		});
-	} else {
-		// SUCCESS!!
-		// replace old list with new one
-		products = updatedListProducts;
-
-		// return updated list
-		res.json(products);
-	}
-});
-
-// DELETE a product
-server.delete("/products/:id", (req, res) => {
-	const productId = Number(req.params.id);
-	console.log("Delete product with id: ", productId);
-
-	// filter list copy, by excluding item to delete
-	const filteredList = products.filter(_product => _product.id !== productId);
-
-	if (filteredList.length === products.length) {
-		res.json({
-			error: 'Product not found'
-		})
-	} else {
-		// SUCCESS!
-		products = filteredList;
-		res.json(products);
-	}
-});
+server.use("/", productsRouter);
 
 // # Foods REST API
 
