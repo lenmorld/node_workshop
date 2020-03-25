2d59f0d4c2819e925032b653dfaa99824b487e4d
lenmorld <lenmorld@gmail.com>
2020-03-25 08:07:25 -0400

    users param in getNextId

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ console.log(users[0]);
 
 const port = 4000;
 
-const getNextId = () => {
+const getNextId = (_users) => {
 	// solution 1: based on users length
 	// return users.length + 1;
 
 	// solution 2: based on max user.id
 	let maxId = 1;
 
-	users.forEach(_user => {
+	_users.forEach(_user => {
 		if (_user.id > maxId) {
 			maxId = _user.id;
 		}
@@ -286,7 +286,7 @@ server.post("/users", (req, res) => {
 		// add new user to users array
 		users.push({
 			...user,
-			id: getNextId(),
+			id: getNextId(users),
 		})
 
 		// return updated list
