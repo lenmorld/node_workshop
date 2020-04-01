f817c06c4278093233626d7fe7ec2b34906a8f4b
lenmorld <lenmorld@gmail.com>
2020-03-31 21:47:21 -0400

    user solutions

--- routes/users.js
+++ routes/users.js
@@ -13,12 +13,14 @@ console.log(users[0]);
 const DbConnection = require('../db');
 
 // GET all users
-router.get("/users", (req, res) => {
-	// ### C4_CHALLENGE ###
+router.get("/users", async (req, res) => {
+	const dbCollection = await DbConnection.getCollection("users");
+	const users = await dbCollection.find().toArray();
+	res.json(users);
 });
 
 // GET one user identified by id
-router.get("/users/:id", (req, res) => {
+router.get("/users/:id", async (req, res) => {
 	const userId = Number(req.params.id);
 	const dbCollection = await DbConnection.getCollection("users");
 	const user = await dbCollection.findOne({ id: userId });
@@ -26,28 +28,74 @@ router.get("/users/:id", (req, res) => {
 });
 
 // POST (create) a user 
-router.post("/users", (req, res) => {
-	const user = req.body;
-	console.log('Adding new user: ', user);
+router.post("/users", async (req, res) => {
+	const newUser = req.body;
+	console.log('Adding new user: ', newUser);
 
-	// ### C4 CHALLENGE ###
+	const dbCollection = await DbConnection.getCollection("users");
+	const user = await dbCollection.findOne({ id: newUser.id });
+
+	if (user) {
+		res.json({
+			error: "User with given id already exists"
+		})
+	} else {
+		let users = await dbCollection.find().toArray();
+
+		await dbCollection.insertOne({
+			...newUser,
+			id: crudHelper.getNextId(users),
+			createdAt: dateTimeHelper.getTimeStamp(),
+		});
+
+		// return updated list
+		users = await dbCollection.find().toArray();
+		res.json(users);
+	}
 });
 
 // PUT (update) a user
-router.put("/users/:id", (req, res) => {
+router.put("/users/:id", async (req, res) => {
 	const userId = Number(req.params.id);
 	const updatedUser = req.body;
 	console.log("Editing user ", userId, " to be ", updatedUser);
 
-	// ### C4 CHALLENGE ###
+	const dbCollection = await DbConnection.getCollection("users");
+	const user = await dbCollection.findOne({ id: userId });
+
+	if (!user) {
+		res.json({
+			error: "User with given id doesn't exist"
+		})
+	}
+
+	updatedUser.updatedAt = dateTimeHelper.getTimeStamp();
+	await dbCollection.updateOne({ id: userId }, { $set: updatedUser });
+
+	// return updated list
+	const users = await dbCollection.find().toArray();
+	res.json(users);
 });
 
 // DELETE a user
-router.delete("/users/:id", (req, res) => {
+router.delete("/users/:id", async (req, res) => {
 	const userId = Number(req.params.id);
 	console.log("Delete user with id: ", userId);
 
-	// ### C4 CHALLENGE ###
+	const dbCollection = await DbConnection.getCollection("users");
+	const user = await dbCollection.findOne({ id: userId });
+
+	if (!user) {
+		res.json({
+			error: "User with given id doesn't exist"
+		})
+	}
+
+	await dbCollection.deleteOne({ id: userId });
+
+	// return updated list
+	const users = await dbCollection.find().toArray();
+	res.json(users);
 });
 
 module.exports = router; 
