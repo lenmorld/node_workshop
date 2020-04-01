c2a9fbea6bbb2422ed191753302f51bc477635a7
lenmorld <lenmorld@gmail.com>
2020-03-31 21:50:34 -0400

    include post

--- routes/users.js
+++ routes/users.js
@@ -27,10 +27,29 @@ router.get("/users/:id", (req, res) => {
 
 // POST (create) a user 
 router.post("/users", (req, res) => {
-	const user = req.body;
-	console.log('Adding new user: ', user);
+	const newUser = req.body;
+	console.log('Adding new user: ', newUser);
 
-	// ### C4 CHALLENGE ###
+	const dbCollection = await DbConnection.getCollection("users");
+	const user = await dbCollection.findOne({ id: newUser.id });
+
+	if (user) {
+		res.json({
+			error: "User with given id already exists"
+		})
+	} else {
+		let users = await dbCollection.find().toArray();
+
+		await dbCollection.insertOne({
+			...newUser,
+			id: crudHelper.getNextId(users),
+			createdAt: dateTimeHelper.getTimeStamp(),
+		});
+
+		// return updated list
+		users = await dbCollection.find().toArray();
+		res.json(users);
+	}
 });
 
 // PUT (update) a user
