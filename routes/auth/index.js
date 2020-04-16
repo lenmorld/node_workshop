1fcaad5a548ee97d21a15e23348912cbde3592ae
lenmorld <lenmorld@gmail.com>
2020-04-15 20:49:54 -0400

    config

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -7,9 +7,6 @@ const jwt = require('jsonwebtoken');
 // import config file
 const config = require('../../config');
 
-// import config file
-const config = require('../../config');
-
 // import modules
 const crudHelper = require('../../utils/crudHelper');
 const dateTimeHelper = require('../../utils/dateTimeHelper');
