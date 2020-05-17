1ef2ad30f6a199ebfe87bad3510a2a95df8d2ce3
lenmorld <lenmorld@gmail.com>
2020-05-17 18:58:09 -0400

    complete step 1,2,3,4 OAuth1 to request user info

--- oauth1/index.js
+++ oauth1/index.js
@@ -14,7 +14,14 @@ server.use(body_parser.urlencoded()) // parse HTML form data
 
 server.set('view engine', 'ejs')
 
+// STEP 1
 const request_url = `https://api.twitter.com/oauth/request_token`
+// STEP 3
+const access_token_url = 'https://api.twitter.com/oauth/access_token'
+
+// Possible requests
+const show_user_url = 'https://api.twitter.com/1.1/users/show.json'
+
 const consumer_key = '8ZWnKZhuS6PGbGhizrGFVOy54'
 const consumer_secret = 'WhDUjBtypCVfhEUt5T5Wp9T875JfEqjcFfGKKKc5S05MJScg0v'
 // const request_token_callback = encodeURIComponent('http://localhost:3001/callback')
@@ -23,40 +30,62 @@ const consumer_secret = 'WhDUjBtypCVfhEUt5T5Wp9T875JfEqjcFfGKKKc5S05MJScg0v'
 // const request_token_callback = 'https://8c2545a5.ngrok.io/callback'
 const request_token_callback = 'http://localhost:3001/callback'
 
+server.get("/", (req, res) => {
+	res.render('step1')
+})
 
+/**
+ * STEP 1: POST oauth/request_token
+ * create a request for an app to obtain a request token
+ * result: oauth_token
+ */
+
+/**
+ * WIP: manually generating the POST data for oauth
+ * 😢 CANT MAKE THIS WORK YET
+ */
 function getAuthRequestString() {
 	const crypto = require('crypto');
-	let nonce = crypto.randomBytes(16).toString('base64');
+	// let nonce = crypto.randomBytes(16).toString('base64');
+	let nonce = crypto.randomBytes(16).toString('hex'); // so only a-f,0-9
 
 	const encoded_callback = encodeURIComponent(request_token_callback)
 
 	const timestamp = Date.now()
 
-	const signature = oauth_sign.hmacsign('POST', request_url,
+	// const signature = oauth_sign.hmacsign('POST', request_url,
+	// 	{
+	// 		oauth_callback: encoded_callback,
+	// 		oauth_consumer_key: consumer_key,
+	// 		oauth_nonce: nonce,
+	// 		oauth_signature: 'HMAC-SHA1',
+	// 		oauth_timestamp: timestamp,
+	// 		oauth_version: '1.0'
+	// 	},
+	// 	consumer_secret)
+
+	const signature = oauth_sign.sign(
+		"HMAC-SHA1",
+		'POST',
+		request_url,
 		{
-			oauth_callback: encoded_callback,
+			oauth_callback: request_token_callback,
 			oauth_consumer_key: consumer_key,
 			oauth_nonce: nonce,
-			oauth_signature: 'HMAC-SHA1',
-			oauth_timestamp: timestamp,
-			oauth_version: '1.0'
+			oauth_signature_method: "HMAC-SHA1",
+			oauth_timestamp: "${timestamp}",
+			oauth_version: "1.0"
 		},
 		consumer_secret)
 
-	const string = `OAuth oauth_nonce="${nonce}",oauth_callback="${encoded_callback}",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${timestamp}", oauth_consumer_key="${consumer_key}",oauth_signature="${signature}",oauth_version="1.0"`
+	const encoded_signature = encodeURIComponent(signature)
+
+	const string = `OAuth oauth_callback="${encoded_callback}",oauth_consumer_key="${consumer_key}",oauth_nonce="${nonce}",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${timestamp}",oauth_version="1.0",oauth_signature="${encoded_signature}"`
 
 	// const string = `OAuth oauth_nonce="${nonce}", oauth_callback="${callback}", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${timestamp}", oauth_consumer_key="${consumer_key}", oauth_signature="${signature}", oauth_version = "1.0"`
 
 	return string
 }
-
-server.get("/", (req, res) => {
-	res.render('index')
-})
-
-/**
- * WIP: manually generating the POST data for oauth
- */
 server.get("/request_token", (req, res) => {
 	const authString = getAuthRequestString()
 
@@ -64,8 +93,8 @@ server.get("/request_token", (req, res) => {
 
 	const headers = {
 		// 'Content-Type': 'application/json',
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Authorization': authString
+		// 'Content-Type': 'application/x-www-form-urlencoded',
+		Authorization: authString
 	}
 
 	axios.post(request_url, {}, {
@@ -88,27 +117,69 @@ server.get("/request_token", (req, res) => {
 
 /**
  * uses request's built-in OAuth1 signing, etc
+ * ✅ wORKS FOR STEP 1
  */
 server.get("/request_token_2", (req, res) => {
-	const oauth_step_1_data = {
-		callback: request_token_callback
-		, consumer_key: consumer_key
-		, consumer_secret: consumer_secret
+	const access_token = server.get('access_token')
+	const access_token_secret = server.get('access_token_secret')
+	// const screen_name = server.get('screen_name')
+	// const user_id = server.get('user_id')
+
+	if (access_token && access_token_secret) {
+		console.log("CACHE exists for access_token && access_token_secret 🎉. Skip STEP 1,2,3. Go straight to API request")
+
+		res.redirect('/user')
+
+		server.set('access_token_available_message', "Access token still good 👍🏽👍🏽👍🏽")
+
+		return
 	}
 
-	console.log(oauth_step_1_data)
+	// if cached oauth_token and oauth_token_secret
+	const oauth_token = server.get('oauth_token')
+	const oauth_token_secret = server.get('oauth_token_secret')
+
+	if (oauth_token && oauth_token_secret) {
+		console.log("CACHE exists for oauth_token && oauth_token_secret 🎉. Skip STEP 1")
+
+		res.render('step2', {
+			// message: JSON.stringify(req_data),
+			message: "Request token still good 👍🏽",
+			redirect_url: `https://api.twitter.com/oauth/authorize?${oauth_token}`
+		})
+
+		return
+	}
+
+	const oauth_step_1_data = {
+		callback: request_token_callback,
+		consumer_key: consumer_key,
+		consumer_secret: consumer_secret
+	}
+
+	console.log("Oauth step 1 data:", oauth_step_1_data)
 
 	request.post({ url: request_url, oauth: oauth_step_1_data }, function (e, r, body) {
 		if (e) {
 			res.json({
+				message: 'step1 failed',
 				error: e
 			})
 		} else {
 			const req_data = qs.parse(body)
 			console.log(req_data.oauth_token)
 
-			res.json({
-				message: req_data
+			const oauth_token = qs.stringify({
+				oauth_token: req_data.oauth_token
+			})
+
+			// CACHE these in server
+			server.set('oauth_token', req_data.oauth_token)
+			server.set('oauth_token_secret', req_data.oauth_token_secret)
+
+			res.render('step2', {
+				message: JSON.stringify(req_data),
+				redirect_url: `https://api.twitter.com/oauth/authorize?${oauth_token}`
 			})
 
 			/*
@@ -118,12 +189,191 @@ server.get("/request_token_2", (req, res) => {
 	})
 })
 
+
+/**
+ * STEP 2: GET oauth/authorize
+ * User authenticates via a sign-in button on Twitter
+ * and redirects to callback_url 
+ * The app then sends request token from step 1
+ * 
+ */
 server.get("/callback", (req, res) => {
-	console.log(req.query)
-	res.json({
-		"message": "callback"
+	console.log("params from Step 2 callback", req.query)
+
+	const { oauth_token, oauth_verifier } = req.query;
+
+	// this oauth_token should be same with token we got before
+	if (oauth_token !== server.get('oauth_token')) {
+		res.json({
+			error: 'oauth_token from step 2 not same with step 1'
+		})
+	} else {
+		// TODO: get oauth_token and oauth_verifier from query string
+		// res.json({
+		// 	"message": req.query
+		// })
+		exchangeRequestTokenToAccessToken({ oauth_token, oauth_verifier, res })
+	}
+
+	/*
+	{"message":{"oauth_token":"E0fX8QAAAAABEanCAAABch6inzI","oauth_verifier":"cGgjOcVeu7dtK6nAE0PUv3IXezX5VmkU"}}
+	*/
+})
+
+/**
+ * STEP 3: POST oauth/access_token
+ * exchange request token into an access token
+ */
+function exchangeRequestTokenToAccessToken({
+	oauth_token, oauth_verifier, res
+}) {
+	/*
+	this one fails =(
+		curl -X POST --header 'Authorization: OAuth oauth_consumer_key=8ZWnKZhuS6PGbGhizrGFVOy54 oauth_token=E0fX8QAAAAABEanCAAABch6inzI oauth_verifier=cGgjOcVeu7dtK6nA
+		E0PUv3IXezX5VmkU' -v https://api.twitter.com/oauth/access_token
+	*/
+
+	/*
+	shortcut: http://localhost:3001/callback?oauth_token=E0fX8QAAAAABEanCAAABch6inzI&oauth_verifier=cGgjOcVeu7dtK6nAE0PUv3IXezX5VmkU
+	*/
+
+	console.log("POST: exchange oauth_token for access_token")
+
+	request.post({
+		url: access_token_url,
+		oauth: {
+			consumer_key: consumer_key,
+			consumer_secret: consumer_secret,
+			token: oauth_token,
+			token_secret: server.get('oauth_token_secret'),
+			verifier: oauth_verifier
+		}
+	}, function (e, r, body) {
+		if (e) {
+			res.json({
+				message: 'step3 failed',
+				error: e
+			})
+		} else {
+			const req_data = qs.parse(body)
+
+			// CACHE user's access_token and authorization details
+			server.set('access_token', req_data.oauth_token)
+			server.set('access_token_secret', req_data.oauth_token_secret)
+			server.set('screen_name', req_data.screen_name)
+			server.set('user_id', req_data.user_id)
+
+			// showUserPageWithDetails(req_data, res);
+
+			res.redirect("/user");
+			/*
+			{"message":{"oauth_token":"1261716580687400966-TNhdJG6XDCfJmty0tw6QYGEq420zQA","oauth_token_secret":"6mKaJKXUfQRiIYu7vyzz1eJIeMXVB0mWulGWb9j1FDQ8X","user_id":"1261716580687400966","screen_name":"TheBotofLenny1"}}
+			*/
+
+			// res.json({
+			// 	message: req_data
+			// })
+
+		}
+	})
+}
+
+// STEP 4 - Make a request! ==============
+server.get('/user', (req, res) => {
+	const access_token = server.get('access_token')
+	const access_token_secret = server.get('access_token_secret')
+	const screen_name = server.get('screen_name')
+	const user_id = server.get('user_id')
+	const access_token_available_message = server.get('access_token_available_message')
+
+	const user_details = {
+		access_token,
+		access_token_secret,
+		screen_name,
+		user_id,
+	}
+
+	requestWithAccessToken(user_details, show_user_url).then(
+		user_info => {
+			res.render('user', {
+				user_info: user_info,
+				additional_message: access_token_available_message
+			})
+		}
+	).catch(e => {
+		res.json({
+			message: `Error in request on ${show_user_url}`,
+			error: e
+		})
 	})
 })
+
+// function showUserPageWithDetails(user_data, res, additional_message) {
+// 	const user_details = {
+// 		screen_name: user_data.screen_name,
+// 		user_id: user_data.user_id
+// 	}
+
+// 	requestWithAccessToken(user_details, show_user_url).then(
+// 		user_info => {
+// 			res.render('user', {
+// 				user_info: user_info,
+// 				additional_message: additional_message
+// 			})
+// 		}
+// 	).catch(e => {
+// 		res.json({
+// 			message: `Error in request on ${show_user_url}`,
+// 			error: e
+// 		})
+// 	})
+// 	// =======================================
+// }
+
+
+function requestWithAccessToken(user_details, request_url) {
+	return new Promise((resolve, reject) => {
+		const {
+			access_token,
+			access_token_secret,
+			screen_name,
+			user_id
+		} = user_details
+
+		console.log("Request using access token")
+
+		request.get({
+			url: request_url,
+			oauth: {
+				consumer_key: consumer_key,
+				consumer_secret: consumer_secret,
+				token: access_token,
+				token_secret: access_token_secret
+			},
+			qs: {
+				screen_name: screen_name,
+				user_id: user_id
+			},
+			json: true,
+		}, function (e, r, result) {
+			if (e) {
+				reject(e)
+			} else {
+				// request-lib might have the errors here
+				if (result.errors) {
+					reject(result.errors)
+				} else {
+					console.log(result)
+					// TODO: render user info in a page
+					resolve(result)
+				}
+			}
+
+		})
+	})
+}
+
+
 
 server.listen(port, () => { // Callback function in ES6
 	console.log(`Server listening at ${port}`);
