6820bb2f278e7c35be5ca72bb2b2472284f1248b
lenmorld <lenmorld@gmail.com>
2020-06-04 07:57:27 -0400

    auth for api using JWT

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session');
+const jwt = require('jsonwebtoken');
 
 // import config file
 const config = require('../../config');
@@ -144,6 +145,81 @@ router.get('/logout', (req, res) => {
 	}
 
 	res.redirect('login');
+});
+
+// ### API auth ###
+// POST credentials for API auth
+router.post("/api/token", async (req, res) => {
+	// credentials passed as POST body
+	const userToAuth = req.body;
+
+	if (!userToAuth.username || !userToAuth.password) {
+		return res.json({
+			error: "Username and password required"
+		})
+	}
+
+	console.log(`Authenticating ${userToAuth.username}`)
+
+	// find user from DB
+	const dbCollection = await DbConnection.getCollection("users");
+	const user = await dbCollection.findOne({
+		username: userToAuth.username,
+	});
+
+	if (!user) {
+		return res.json({
+			error: "Login failed"
+		})
+	}
+
+	// check if given password's hash matches the user password hash in the DB
+	const isMatch = await bcrypt.compare(userToAuth.password, user.password);
+	console.log(`Plain text: ${userToAuth.password}`)
+	console.log(`Hash: ${user.password}`)
+	console.log(`match: ${isMatch}`)
+
+	if (isMatch) {
+		// JWT: create token from user details
+		const payload = { username: userToAuth.username };
+		const token_expiry = '24h';
+		const token = jwt.sign(payload, config.secret_key, {
+			expiresIn: token_expiry
+		});
+
+		res.json({ message: "Auth successful", auth: true, token: token });
+	} else {
+		res.json({
+			message: "Login failed"
+		})
+	}
+});
+
+// GET authenticated resource
+router.get("/api/resource", async (req, res) => {
+	// console.log(req.headers);
+	const token = req.headers['x-access-token'] || req.headers['authorization'];
+	if (!token) {
+		// 401 = unauthorized
+		return res.status(401).json({
+			message: "No token specified"
+		})
+	}
+
+	jwt.verify(token, config.secret_key, async (err, decoded) => {
+		if (err) {
+			// 401 = unauthorized then return token validation error
+			res.status(401).json({
+				error: err.name,
+				message: err.message,
+			});
+		} else {
+			// SUCCESSFUL AUTH
+			const dbCollection = await DbConnection.getCollection("foods");
+			const foods = await dbCollection.find().toArray();
+			res.json(foods);
+		}
+	});
 });
 
 module.exports = router; 
