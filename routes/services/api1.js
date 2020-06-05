428e4ac6c22873906e6b41caafdc7ded409935c5
lenmorld <lenmorld@gmail.com>
2020-06-05 12:17:17 -0400

    no auth - Github Jobs API 1

--- routes/services/api1.js
+++ routes/services/api1.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 // external API routes
+
+// JSON typicode: sample blog posts
 router.get("/services/posts/:id", function (req, res) {
 	const id = req.params.id;
 	axios(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -11,6 +13,23 @@ router.get("/services/posts/:id", function (req, res) {
 		}).catch(err => {
 			throw err;
 		});
+});
+
+// Github Jobs API
+router.get("/services/jobs", function (req, res) {
+	const { description, location } = req.query;
+	let requestUrl = "https://jobs.github.com/positions.json";
+	if (description || location) {
+		const descriptionParam = description ? `description=${description}&` : '';
+		const locationParam = location ? `location=${location}&` : '';
+		requestUrl = `${requestUrl}?${descriptionParam}${locationParam}`;
+	}
+	console.log(`Request: ${requestUrl}`);
+	axios(requestUrl).then(result => {
+		res.json(result.data);
+	}).catch(err => {
+		throw err;
+	});
 });
 
 module.exports = router;
